092e022eec1763ac8d33ebe437835cd56ffcb75f
SeanBrennan645git <16020316@students.southwales.ac.uk>
2021-08-02 18:56:54 +0100

    Setup of Building Escape project
    Created new project to be used to the next section of course

---
+++ BuildingEscape/Intermediate/Build/Win64/BuildingEscapeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30038
+#include "C:/Users/Sean Brennan/UnrealEngineCourse/UnrealEngineCourseProjects/BuildingEscape/Intermediate/Build/Win64/BuildingEscapeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
